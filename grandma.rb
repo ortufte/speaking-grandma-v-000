114f1d22c382c5d11e660b9a2695c1fe89dfdba1
Olivia <ortufte@gmail.com>
2019-09-10 20:48:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else 
     return "NO, NOT SINCE 1938!"
